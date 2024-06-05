e8a585547cb49ea87ce59a0a479ef4132e37f237
shifatx62 <shifatx62@gmail.com>
2024-06-05 22:31:37 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+const burger = document.querySelector('.navbar-burger');
+        const navMobile = document.querySelector('.nav-items-mobile');
+
+        burger.addEventListener('click', () => {
+            navMobile.classList.toggle('hidden');
+        });
